b835e95972ab6433a31f7c5d6e6954a1b0412df4
Dat Chung <datchung@users.noreply.github.com>
2019-11-30 22:48:16 -0500

    Implement update record in RecordApi (WIP)

--- src/Api/RecordApi.js
+++ src/Api/RecordApi.js
@@ -8,14 +8,30 @@ export function getRecords() {
 export function saveRecord(record) {
   var records = RecordPersistence.getSavedRecords();
 
-  if(!record.id) {
+  if(!record.id || !records) {
     // Add new record
     record.id = uuid();
+    record.dateCreated = + new Date();
+    record.dateModified = + record.dateCreated;
     saveRecords([...records, record]);
     return record;
   }
 
   // Update existing record
+  var modifiedRecords = records.map(r => {
+    if(r.id !== record.id) return r;
+    
+    record = {
+      id: r.id,
+      text: record.text,
+      dateCreated: r.dateCreated,
+      dateModified: + new Date()
+    };
+    return record;
+  });
+
+  RecordPersistence.saveRecords(modifiedRecords);
+  return record;
 }
 
 function saveRecords(records) {
--- src/Components/Records/RecordForm.js
+++ src/Components/Records/RecordForm.js
@@ -1,11 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import TextInput from "../Common/TextInput";
-// import SelectInput from "../Common/SelectInput";
 
 const RecordForm = ({
   record,
-  // authors,
   onSave,
   onChange,
   saving = false,
@@ -27,27 +25,6 @@ const RecordForm = ({
         error={errors.text}
       />
 
-      {/* <SelectInput
-        name="authorId"
-        label="Author"
-        value={record.authorId || ""}
-        defaultOption="Select Author"
-        options={authors.map(author => ({
-          value: author.id,
-          text: author.name
-        }))}
-        onChange={onChange}
-        error={errors.author}
-      /> */}
-
-      {/* <TextInput
-        name="category"
-        label="Category"
-        value={record.category}
-        onChange={onChange}
-        error={errors.category}
-      /> */}
-
       <button type="submit" disabled={saving} className="btn btn-primary">
         {saving ? "Saving..." : "Save"}
       </button>
@@ -56,7 +33,6 @@ const RecordForm = ({
 };
 
 RecordForm.propTypes = {
-  // authors: PropTypes.array.isRequired,
   record: PropTypes.object.isRequired,
   errors: PropTypes.object,
   onSave: PropTypes.func.isRequired,
