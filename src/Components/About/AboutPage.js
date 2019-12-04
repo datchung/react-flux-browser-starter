1a1ac9b998155a02eb4572c541e68bc8097132fa
Dat Chung <datchung@users.noreply.github.com>
2019-12-03 21:50:38 -0500

    Style home and about page

--- src/Components/About/AboutPage.js
+++ src/Components/About/AboutPage.js
@@ -1,13 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Back from "../Common/Back";
+import PageTitle from "../Common/PageTitle";
 
-const AboutPage = () => (
-  <div>
-    <h2>About</h2>
-    <p>
-      This app uses React, Redux, React Router, and many other helpful
-      libraries.
-    </p>
-  </div>
+const AboutPage = (props) => (
+  <>
+    <Back history={props.history} />
+    <PageTitle title="About" />
+    <p>This app uses React, Redux, React Router, and many other helpful
+      libraries.</p>
+  </>
 );
+
+AboutPage.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 
 export default AboutPage;
--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import Back from "../Common/Back";
+import PageTitle from "../Common/PageTitle";
 
-const HomePage = () => (
-  <div className="jumbotron">
-    <h1>Records App</h1>
+const HomePage = (props) => (
+  <>
+    <Back history={props.history} />
+    <PageTitle title="Record App" />
     <p>React, Redux and React Router for ultra-responsive web apps.</p>
     <Link to="about" className="btn btn-primary btn-lg">
       Learn more
     </Link>
-  </div>
+  </>
 );
+
+HomePage.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 
 export default HomePage;
